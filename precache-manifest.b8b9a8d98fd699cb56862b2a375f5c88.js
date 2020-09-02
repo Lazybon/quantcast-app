59b41d899d157241585bfd1018472e9b47a252fe
Alexey Shemyakov <IT.shemyakov@ya.ru>
2020-09-02 12:27:13 +0400

    Updates

---
+++ precache-manifest.b8b9a8d98fd699cb56862b2a375f5c88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ddc12aef08727c4402658bdb18a08e",
+    "url": "/quancast-app/index.html"
+  },
+  {
+    "revision": "037db55ac1357cc44d7e",
+    "url": "/quancast-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1738f260cf37cde0b6c4",
+    "url": "/quancast-app/static/js/2.68e391af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quancast-app/static/js/2.68e391af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037db55ac1357cc44d7e",
+    "url": "/quancast-app/static/js/main.bcfc153f.chunk.js"
+  },
+  {
+    "revision": "b42ae879b474a04d0a1d",
+    "url": "/quancast-app/static/js/runtime-main.9d1fc317.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quancast-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
